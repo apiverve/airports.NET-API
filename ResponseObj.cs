41990b3460ee9e2b786e29a3348c3af1ac1a82a6
APIVerve Team <hello@apiverve.com>
2024-05-14 15:07:57 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,69 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class cityinfo
+{
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("altName")]
+    public string altName { get; set; }
+
+    [JsonProperty("country")]
+    public string country { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("icao")]
+    public string icao { get; set; }
+
+    [JsonProperty("iata")]
+    public string iata { get; set; }
+
+    [JsonProperty("name")]
+    public string name { get; set; }
+
+    [JsonProperty("city")]
+    public string city { get; set; }
+
+    [JsonProperty("state")]
+    public string state { get; set; }
+
+    [JsonProperty("country")]
+    public string country { get; set; }
+
+    [JsonProperty("elevation")]
+    public int elevation { get; set; }
+
+    [JsonProperty("lat")]
+    public double lat { get; set; }
+
+    [JsonProperty("lon")]
+    public double lon { get; set; }
+
+    [JsonProperty("tz")]
+    public string tz { get; set; }
+
+    [JsonProperty("city_info")]
+    public cityinfo cityinfo { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
